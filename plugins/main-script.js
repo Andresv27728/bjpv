10a0d14870a67ab7dd00c9746c82d5846ec13f1d
bakukats07 <crisvegar2@gmail.com>
2025-01-31 21:23:53 -0500

    main-script.js

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { conn, args }) => {
 try {
-let res = await fetch('https://github.com/2277meliodas/Mitsuri-Kanroji-Bot-MD/tree/main')
+let res = await fetch('https://github.com/2277meliodas/Mitsuri-Kanroji-Bot-MD')
 
 if (!res.ok) throw new Error('Error al obtener datos del repositorio')
 let json = await res.json()
