b8571626317e04d764b5dcccf89b065a6f751e24
bakukats07 <crisvegar2@gmail.com>
2025-02-02 02:13:50 -0500

    grupo-unbanchat.js

--- plugins/grupo-unbanchat.js
+++ plugins/grupo-unbanchat.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn }) => {
-if (!(m.chat in global.db.data.chats)) return conn.reply(m.chat, '🍭l🍬 *¡Este chat no está registrado!*', m, fake)
+if (!(m.chat in global.db.data.chats)) return conn.reply(m.chat, '💐l🍡 *¡Este chat no está registrado!*', m, fake)
 let chat = global.db.data.chats[m.chat]
-if (!chat.isBanned) return conn.reply(m.chat, '🍭 *¡Yuki-Bot no está baneada en este chat!*', m, fake)
+if (!chat.isBanned) return conn.reply(m.chat, '💐𝑴𝒊𝒕𝒔𝒖𝒓𝒊 𝑲𝒂𝒏𝒓𝒐𝒋𝒊-𝑴𝑫 *no está baneada en este chat!*', m, fake)
 chat.isBanned = false
-await conn.reply(m.chat, '🍬 *¡Yuki-Bot ya fué desbaneada en este chat!*', m, fake)
+await conn.reply(m.chat, '> 🍡 𝑴𝒊𝒕𝒔𝒖𝒓𝒊 𝑲𝒂𝒏𝒓𝒐𝒋𝒊-𝑴𝑫 ya esta desbaneada de este chat ♡', m, fake)
 }
 handler.help = ['unbanchat'];
 handler.tags = ['grupo'];
