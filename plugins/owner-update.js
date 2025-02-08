9cd4c0a519759ec7c604c70f362ac7f14c895467
bakukats07 <crisvegar2@gmail.com>
2025-02-08 15:02:08 -0500

    owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,11 +1,11 @@
 import { exec } from 'child_process';
 
 let handler = async (m, { conn }) => {
-  m.reply('🍭 Actualizando el bot...');
+  m.reply('🌷↛𝐴𝑐𝑡𝑢𝑎𝑙𝑖𝑧𝑎𝑛𝑑𝑜 𝑒𝑙 𝑏𝑜𝑡, 𝑒𝑠𝑝𝑒𝑟𝑒 𝑢𝑛 𝑚𝑜𝑚𝑒𝑛𝑡𝑜_°...');
 
   exec('git pull', (err, stdout, stderr) => {
     if (err) {
-      conn.reply(m.chat, `⚠️ Error: No se pudo realizar la actualización.\nRazón: ${err.message}`, m);
+      conn.reply(m.chat, `✖️ 𝐸𝑅𝑅𝑂𝑅:↛𝑁𝑜 𝑠𝑒 𝑝𝑢𝑑𝑜 𝑟𝑒𝑎𝑙𝑖𝑧𝑎𝑟 𝑙𝑎 𝑎𝑐𝑡𝑢𝑎𝑙𝑖𝑧𝑎𝑐𝑖𝑜𝑛_°.\nRazón: ${err.message}`, m);
       return;
     }
 
@@ -14,9 +14,9 @@ let handler = async (m, { conn }) => {
     }
 
     if (stdout.includes('Already up to date.')) {
-      conn.reply(m.chat, '🍭 El bot ya está actualizado.', m);
+      conn.reply(m.chat, '🍡↛𝐸𝑙 𝑏𝑜𝑡 𝑦𝑎 𝑒𝑠𝑡𝑎 𝑎𝑐𝑡𝑢𝑎𝑙𝑖𝑧𝑎𝑑𝑜_°.', m);
     } else {
-      conn.reply(m.chat, `🍬 Actualización realizada con éxito.\n\n${stdout}`, m);
+      conn.reply(m.chat, `🌷↛𝐴𝑐𝑡𝑢𝑎𝑙𝑖𝑧𝑎𝑐𝑖𝑜𝑛 𝑟𝑒𝑎𝑙𝑖𝑧𝑎𝑑𝑎 𝑐𝑜𝑛 *𝐸𝑋𝐼𝑇𝑂_°*.\n\n${stdout}`, m);
     }
   });
 };
