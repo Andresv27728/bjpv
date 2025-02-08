bbc3581187d8c39b6479afb8397359f195ff8f0c
bakukats07 <crisvegar2@gmail.com>
2025-02-08 16:32:42 -0500

    grupo-config.js

--- plugins/grupo-config.js
+++ plugins/grupo-config.js
@@ -9,15 +9,15 @@ let isClose = { // Switch Case Like :v
 'cerrar': 'announcement',
 }[(args[0] || '')]
 if (isClose === undefined)
-return conn.reply(m.chat, `🍬 *Elija una opción para configurar el grupo*\n\nEjemplo:\n*✰ #${command} abrir*\n*✰ #${command} cerrar*\n*✰ #${command} close*\n*✰ #${command} open*`, m)
+return conn.reply(m.chat, `🌷↛ *𝐸𝑙𝑖𝑗𝑎 𝑙𝑎 𝑜𝑝𝑐𝑖𝑜𝑛 𝑝𝑎𝑟𝑎 𝑐𝑜𝑛𝑓𝑖𝑔𝑢𝑟𝑎𝑟 𝑒𝑙 𝑔𝑟𝑢𝑝𝑜*\n\n𝑃𝑜𝑟 𝑒𝑗𝑒𝑚𝑝𝑙𝑜:\n*✰ #${command} abrir*\n*✰ #${command} cerrar*\n*✰ #${command} close*\n*✰ #${command} open*`, m)
 await conn.groupSettingUpdate(m.chat, isClose)
 
 if (isClose === 'not_announcement'){
-m.reply(`🍬 *Ya pueden escribir en este grupo.*`)
+m.reply(`🌷↛ *𝑌𝑎 𝑝𝑢𝑒𝑑𝑒𝑛 𝑒𝑠𝑐𝑟𝑖𝑏𝑖𝑟 𝑒𝑛 𝑒𝑠𝑡𝑒 𝑔𝑟𝑢𝑝𝑜_°*`)
 }
 
 if (isClose === 'announcement'){
-m.reply(`🍭 *Solos los admins pueden escribir en este grupo.*`)
+m.reply(`🌷↛ *𝑆𝑜𝑙𝑜 𝑙𝑜𝑠 𝑎𝑑𝑚𝑖𝑛𝑠, 𝑝𝑢𝑒𝑑𝑒𝑛 𝑒𝑠𝑐𝑟𝑖𝑏𝑖𝑟 𝑒𝑛 𝑒𝑠𝑡𝑒 𝑔𝑡𝑢𝑝𝑜_°*`)
 }}
 handler.help = ['group open / close', 'grupo abrir / cerrar']
 handler.tags = ['grupo']
