8be1860cc65861a5602f7d8b5302cd80f5e75221
deylinqff <maycraftminecraft4@gmail.com>
2025-02-10 20:49:58 -0600

    Actualizar plugins

---
+++ plugins/tools-imgAi.js
@@ -0,0 +1,26 @@
+// créditos a https://github.com/deylinqff
+import fetch from 'node-fetch';
+
+const handler = async (m, { conn, text }) => {
+  if (!text) throw '*👑 Falta texto para generar imagen*';
+  
+  m.react('✨');
+  await conn.sendMessage(m.chat, { text: '*👑 Generando imagen*' }, { quoted: m });
+
+  try {
+    const res = await fetch(`https://eliasar-yt-api.vercel.app/api/ai/text2img?prompt=${encodeURIComponent(text)}`);
+    if (!res.ok) throw new Error();
+    
+    const buffer = await res.buffer();
+    m.react('🪄');
+    await conn.sendMessage(m.chat, { image: buffer }, { quoted: m });
+  } catch (e) {
+    throw '*⚠️ Error*';
+  }
+};
+
+handler.tags = ['tools'];
+handler.help = ['genearimg'];
+handler.command = ['genearimg', 'imgg'];
+
+export default handler;
