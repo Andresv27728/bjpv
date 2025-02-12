83779ea9ddcd22bf53952426bc981786e3564cb6
bakukats07 <crisvegar2@gmail.com>
2025-02-11 23:19:04 -0500

    info-totalplugins.js

--- plugins/info-totalplugins.js
+++ plugins/info-totalplugins.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
 let totalf = Object.values(global.plugins).filter(
     (v) => v.help && v.tags
   ).length;
-conn.reply(m.chat, `*🌷↛ 𝑁𝑢𝑚𝑒𝑟𝑜 𝑡𝑜𝑡𝑎𝑙 𝑑𝑒 𝑚𝑢𝑠 𝑓𝑢𝑛𝑐𝑖𝑜𝑛𝑒𝑠✨* : ${totalf}`,m)
+conn.reply(m.chat, `*🌷↛ 𝑁𝑢𝑚𝑒𝑟𝑜 𝑡𝑜𝑡𝑎𝑙 𝑑𝑒 𝑚𝑖𝑠 𝑓𝑢𝑛𝑐𝑖𝑜𝑛𝑒𝑠✨* : ${totalf}`,m)
 }
 
 handler.help = ['totalfunciones']
