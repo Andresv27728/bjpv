52c9d9baa395386a024b7f04c6f636ae34951b9a
deylinqff <maycraftminecraft4@gmail.com>
2025-02-14 20:07:46 -0600

    Actualizar plugins

---
+++ plugins/owner-link2.js
@@ -0,0 +1,38 @@
+// Créditos A Deylin
+
+let handler = async (m, { conn, text }) => {
+  // No Quites Los Créditos🚀
+  m.react('🍭');
+
+  const groupLinkPattern = /chat\.whatsapp\.com\/([a-zA-Z0-9]+)/;
+  const match = text.match(groupLinkPattern);
+
+  if (!match) {
+    await conn.sendMessage(m.chat, { text: '❌ *No enviaste un enlace válido de grupo de WhatsApp.*' });
+    return;
+  }
+
+  const groupId = match[1];
+  const message = "〔👑 *KIRITO-BOT* 👑〕\n\n*Enlace recibido correctamente.*";
+
+  try {
+    await conn.groupAcceptInvite(groupId);
+
+    await conn.sendMessage(m.chat, { text: message });
+  } catch (error) {
+    console.error('Error al aceptar el enlace del grupo:', error);
+    await conn.sendMessage(m.chat, { text: '❌ *Hubo un error al intentar unirse al grupo.*' });
+  }
+};
+
+Object.defineProperty(handler, 'alwaysOn', {
+  value: true, 
+  writable: false,
+});
+
+handler.help = ['link2'];
+handler.tags = ['enlace2'];
+handler.command = ['link2'];
+handler.rowner = true:
+
+export default handler;
