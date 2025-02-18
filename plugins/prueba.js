fa03339dda2857dcfd76dbfc28fe83d3b978b8cd
deylinqff <maycraftminecraft4@gmail.com>
2025-02-18 12:31:37 -0600

    Actualizar plugins

---
+++ plugins/prueba.js
@@ -0,0 +1,30 @@
+import PhoneNumber from 'awesome-phonenumber';
+
+async function handler(m, { conn }) {
+    let numcreador = '50488198573'; // Número del creador
+    let ownerJid = `${numcreador}@s.whatsapp.net`;
+    let name = conn.getName(ownerJid) || 'Deylin';
+
+    let vcard = `
+BEGIN:VCARD
+VERSION:3.0
+N:;${name};;;
+FN:${name}
+TEL;waid=${numcreador}:${PhoneNumber('+' + numcreador).getNumber('international')}
+END:VCARD`.trim();
+
+    await conn.sendMessage(m.chat, { 
+        contacts: {
+            displayName: name,
+            contacts: [{ vcard }]
+        }
+    }, { quoted: m });
+}
+
+handler.help = ['owner'];
+handler.tags = ['main'];
+handler.command = ['owner', 'creator', 'creador2', 'dueño'];
+
+export default handler;
+
+// Código creado por Deyin
