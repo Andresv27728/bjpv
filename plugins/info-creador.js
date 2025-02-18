6dde8e803f217f935541b41bdab01da9a96d4650
deylinqff <maycraftminecraft4@gmail.com>
2025-02-18 13:45:39 -0600

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,31 +1,44 @@
-function handler(m) {
-let name = conn.getName(`${numcreador}@s.whatsapp.net`)
-let ownerN = `${numcreador}`
-conn.sendContact(m.chat, [[`${ownerN}@s.whatsapp.net`, `${name}`]], m, {
- contextInfo: { 
- forwardingScore: 2023,
-isForwarded: false, 
- externalAdReply: {  
- title: packname, 
- body: dev, 
- sourceUrl: channel,
- thumbnail: banner,
- thumbnailUrl: banner, 
- mediaType: 1,
- showAdAttribution: true, 
- renderLargerThumbnail: true 
- }
-   }
-     },
-       {
-         quoted: m
-           }
-             );
+import PhoneNumber from 'awesome-phonenumber';
 
+async function handler(m, { conn }) { 
+    let numcreador = '584120515006';
+    let ownerJid = numcreador + '@s.whatsapp.net';
+
+    let name = await conn.getName(ownerJid) || 'Deylin'; 
+    let about = (await conn.fetchStatus(ownerJid).catch(() => {}))?.status || 'Sin descripción';
+
+    let empresa = 'Deylin - Servicios Tecnológicos';
+
+    let vcard = `
+BEGIN:VCARD
+VERSION:3.0
+N:;${name};;;
+FN:${name}
+ORG:${empresa};
+TITLE:CEO & Fundador
+TEL;waid=${numcreador}:${new PhoneNumber('+' + numcreador).getNumber('international')}
+EMAIL:correo@empresa.com
+URL:https://www.tuempresa.com
+NOTE:${about}
+ADR:;;Dirección de tu empresa;;;;
+X-ABADR:ES
+X-ABLabel:Dirección Web
+X-ABLabel:Correo Electrónico
+X-ABLabel:Teléfono de contacto
+X-WA-BIZ-NAME:${name}
+X-WA-BIZ-DESCRIPTION:${about}
+END:VCARD`.trim();
+
+    await conn.sendMessage(m.chat, { 
+        contacts: { 
+            displayName: name, 
+            contacts: [{ vcard }]
+        } 
+    }, { quoted: m });
 }
 
-handler.help = ['owner']
-handler.tags = ['main']
-handler.command = ['owner', 'creator', 'creador', 'dueño'] 
+handler.help = ['owner']; 
+handler.tags = ['main']; 
+handler.command = ['owner', 'creator', 'creador2', 'dueño'];
 
-export default handler
+export default handler;
