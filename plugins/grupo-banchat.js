7ce2e77361327670782449ca2bfe05176476b503
ⁱᵃᵐ|Lm' Avaricia <sanchezazahel1207@gmail.com>
2025-02-21 12:21:07 -0400

    Update grupo-banchat.js

--- plugins/grupo-banchat.js
+++ plugins/grupo-banchat.js
@@ -1,12 +1,12 @@
 let handler = async (m) => {
 
 global.db.data.chats[m.chat].isBanned = true
-conn.reply(m.chat, `> 💐 𝑴𝒊𝒕𝒔𝒖𝒓𝒊 𝑲𝒂𝒏𝒓𝒐𝒋𝒊-𝑴𝑫 fue exitosamente baneada de este chat ♡`, m, rcanal)
+conn.reply(m.chat, `> 💐 𝑴𝒊𝒕𝒔𝒖𝒓𝒊 𝑲𝒂𝒏𝒓𝒐𝒋𝒊-𝑴𝑫 fue  desactivada de este chat ♡`, m, rcanal)
 
 }
 handler.help = ['banchat']
 handler.tags = ['grupo']
-handler.command = ['banchat']
+handler.command = ['off']
 
 handler.botAdmin = true
 handler.admin = true 
