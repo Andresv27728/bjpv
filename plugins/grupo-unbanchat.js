6e48b987e48023f6ac4c570fad0b585df2c920d3
ⁱᵃᵐ|Lm' Avaricia <sanchezazahel1207@gmail.com>
2025-02-21 12:23:49 -0400

    Update grupo-unbanchat.js

--- plugins/grupo-unbanchat.js
+++ plugins/grupo-unbanchat.js
@@ -1,13 +1,13 @@
 let handler = async (m, { conn }) => {
 if (!(m.chat in global.db.data.chats)) return conn.reply(m.chat, '💐l🍡 *¡Este chat no está registrado!*', m, fake)
 let chat = global.db.data.chats[m.chat]
-if (!chat.isBanned) return conn.reply(m.chat, '💐𝑴𝒊𝒕𝒔𝒖𝒓𝒊 𝑲𝒂𝒏𝒓𝒐𝒋𝒊-𝑴𝑫 *no está baneada en este chat!*', m, fake)
+if (!chat.isBanned) return conn.reply(m.chat, '💐𝑴𝒊𝒕𝒔𝒖𝒓𝒊 𝑲𝒂𝒏𝒓𝒐𝒋𝒊-𝑴𝑫 *no está desactivada en este chat!*', m, fake)
 chat.isBanned = false
-await conn.reply(m.chat, '*🍡↛𝑴𝒊𝒕𝒔𝒖𝒓𝒊 𝑲𝒂𝒏𝒓𝒐𝒋𝒊-𝑴𝑫 ya esta desbaneada de este chat ♡*', m, fake)
+await conn.reply(m.chat, '*🍡↛𝑴𝒊𝒕𝒔𝒖𝒓𝒊 𝑲𝒂𝒏𝒓𝒐𝒋𝒊-𝑴𝑫 ya esta activada en este chat ♡*', m, fake)
 }
 handler.help = ['unbanchat'];
 handler.tags = ['grupo'];
-handler.command = ['unbanchat','desbanearchat','desbanchat']
+handler.command = ['on','activar','desbanchat']
 handler.admin = true 
 handler.botAdmin = true
 handler.group = true
