a7e3c5f04e6dfd320b056925592a04b4f0a65f69
ⁱᵃᵐ|Lm' Avaricia <sanchezazahel1207@gmail.com>
2025-02-24 20:27:45 -0400

    Update rpg-daily.js

--- plugins/rpg-daily.js
+++ plugins/rpg-daily.js
@@ -17,7 +17,7 @@ var handler = async (m, { conn }) => {
 Recursos:
 ✨ Xp : *+${exp}*
 💎 Diamantes : *+${d}*
-💸 ${moneda} : *+${coin}*`, m);
+🪙 ${moneda} : *+${coin}*`, m);
 
     global.db.data.users[m.sender].lastclaim = Date.now();
 }
