22f4226946d1d053c9691f4701098f8abfd14174
deylinqff <maycraftminecraft4@gmail.com>
2025-03-01 15:26:32 -0600

    Actualizar prueba.js

--- plugins/prueba.js
+++ plugins/prueba.js
@@ -1,7 +1,9 @@
-// Créditos a https://github.com/deylinqff
+import MessageType from '@whiskeysockets/baileys';
 
-const handler = async (m, { conn }) => {
-  const menuText = `🌟 *Menú Secundario* 🌟
+const handler = async (m, { conn, usedPrefix, command }) => {
+  // Menú Secundario
+  if (command === 'me' || command === 'menú' || command === 'help' || command === 'ayuda') {
+    const menuText = `🌟 *Menú Secundario* 🌟
 
 🔹 Opciones disponibles:
 1️⃣ *Comandos útiles*
@@ -10,29 +12,46 @@ const handler = async (m, { conn }) => {
 
 Presiona un botón para continuar.`;
 
-  const buttons = [  
-    {  
-      buttonId: '.imgg gato',  
-      buttonText: { displayText: '😻 Gato' },  
-      type: 1  
-    },  
-    {  
-      buttonId: '.imgg perro',  
-      buttonText: { displayText: '🐶 Perro' },  
-      type: 1  
-    }  
-  ];  
+    const buttons = [
+      {
+        buttonId: '.imgg gato',
+        buttonText: { displayText: '😻 Gato' },
+        type: 1
+      },
+      {
+        buttonId: '.imgg perro',
+        buttonText: { displayText: '🐶 Perro' },
+        type: 1
+      }
+    ];
 
-  await conn.sendMessage(m.chat, {
-    text: menuText,
-    footer: '📍 Kirito-Bot',
-    buttons: buttons,
-    headerType: 1
-  }, { quoted: m });
+    await conn.sendMessage(m.chat, {
+      text: menuText,
+      footer: '📍 Kirito-Bot',
+      buttons: buttons,
+      headerType: 1
+    }, { quoted: m });
+  }
+
+  // Comando para eliminar sala de juego (Tres en Raya)
+  const room = Object.values(conn.game).find(
+    (room) => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender)
+  );
+  
+  if (command === 'deltictactoe' || command === 'deltt' || command === 'delttt') {
+    if (room == undefined) {
+      return conn.sendButton(m.chat, '🍭 No estás en ninguna partida de Tres en Raya.', '📍 Kirito-Bot', null, [
+        ['Iniciar sala de juego', `${usedPrefix}ttt partida nueva`]
+      ], m);
+    }
+    
+    delete conn.game[room.id];
+    await m.reply('🍭 Se eliminó la sala de juego de Tres en Raya.');
+  }
 };
 
+handler.command = ['me', 'menú', 'help', 'ayuda', 'deltictactoe', 'deltt', 'delttt'];
 handler.help = ['menu'];
 handler.tags = ['main'];
-handler.command = ['me', 'menú', 'help', 'ayuda'];
 
 export default handler;
