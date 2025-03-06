7ae53bece38ecf9128d6b246291a37a3589ae896
deylinqff <maycraftminecraft4@gmail.com>
2025-03-05 21:37:36 -0600

    Actualizar prueba.js

--- plugins/prueba.js
+++ plugins/prueba.js
@@ -1,7 +1,8 @@
+
 import PhoneNumber from 'awesome-phonenumber';
 
 async function handler(m, { conn }) { 
-    let numcreador = '584120515006';
+    let numcreador = ['584120515006','50488198573'];
     let ownerJid = numcreador + '@s.whatsapp.net';
 
     let name = await conn.getName(ownerJid) || 'Deylin'; 
@@ -19,24 +20,17 @@ TITLE:CEO & Fundador
 TEL;waid=${numcreador}:${new PhoneNumber('+' + numcreador).getNumber('international')}
 EMAIL:correo@empresa.com
 URL:https://www.tuempresa.com
+NOTE:${about}
 ADR:;;Dirección de tu empresa;;;;
 X-ABADR:ES
 X-ABLabel:Dirección Web
 X-ABLabel:Correo Electrónico
 X-ABLabel:Teléfono de contacto
 X-WA-BIZ-NAME:${name}
+X-WA-BIZ-DESCRIPTION:${about}
 END:VCARD`.trim();
 
-    // Enviar vCard junto con la descripción de la empresa
-    let mensaje = `
-¡Hola! Aquí está la información de mi empresa:
-*Empresa:* ${empresa}
-*Descripción:* ${about}
-
-A continuación, te envío mi vCard de contacto.`;
-
     await conn.sendMessage(m.chat, { 
-        text: mensaje, // Enviar la descripción primero
         contacts: { 
             displayName: name, 
             contacts: [{ vcard }]
