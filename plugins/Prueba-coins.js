a0d2a87f40895a9c7025db5be6f8cbfe54fc34c1
ⁱᵃᵐ|Lm' Avaricia <sanchezazahel1207@gmail.com>
2025-03-07 22:14:22 -0400

    Update Prueba-coins.js

--- plugins/Prueba-coins.js
+++ plugins/Prueba-coins.js
@@ -1,4 +1,4 @@
-```canje
+
 const { Client, LocalAuth } = require('whatsapp-web.js');
 
 let codes = {};
@@ -65,4 +65,3 @@ export default handler
 
 function pickRamdom(list) {
 return list[Math.floor(Math.ramdom() * list.length)]}
-```
